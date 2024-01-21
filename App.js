e62f635421b945dd3ca1484aa3f73c669a5f5588
fullstacksherpa <fullstacksherpa@gmail.com>
2024-01-20 19:21:06 -0800

    initial layout with goal item display functionality added

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StyleSheet, Text, View, Button } from "react-native";
-import { Switch } from "react-native-web";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
+import uuid from "react-native-uuid";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, enteredGoalText]);
+    console.log(courseGoals);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={{ margin: 16, borderWidth: 2, borderColor: "red", padding: 16 }}>Hello World!!!!</Text>
-      <Button title="tap me" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="your course goal!" onChangeText={goalInputHandler} />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalContainer}>
+        {courseGoals.map((goal) => (
+          <Text key={uuid.v4()}>{goal}</Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: "#fff",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+    marginBottom: 24,
+    flex: 1,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalContainer: {
+    flex: 5,
   },
 });
